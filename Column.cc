53ffc9f6500cf604f83b8024c61493e482c932e2
yen <yenebeb12@hotmail.com>
2017-04-08 18:01:03 +0200

    foutje

--- Column.cc
+++ Column.cc
@@ -7,8 +7,6 @@
 using namespace std;
 Column::Column(int size){
     for(int i =0; i < size; i++){
-        Cell cel;
-        Cell c(cel);
         cellen.push_back(Cell());
     }
 }
@@ -18,9 +16,9 @@ Cell Column::getCell(int x){
 }
 
 vector<Cell>::iterator Column::begin(){
-    //return cellen.begin();
+    return cellen.begin();
 }
 
 vector<Cell>::iterator Column::end(){
-    //return cellen.end();
+    return cellen.end();
 }
