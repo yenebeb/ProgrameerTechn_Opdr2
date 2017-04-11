0ce7ae4abab68bdac8004fd11fe82179ec917cff
Michiel Vrins <Michiel_678@hotmail.com>
2017-04-11 19:25:29 +0200

    highlight

--- SheetView.h
+++ SheetView.h
@@ -12,13 +12,13 @@ public:
 	void tekeninh(Sheet* sheet);
 	void tekencurs();
 	std::vector<int> getcursor();
-	void setcursor();
+	void setcursor(std::vector<int> vec);
 	WINDOW* getWindow();
 	
 
 private:
 	WINDOW *win;
-	std::vector<int> cursor = {1,10};
+	std::vector<int> cursor = {0,0};
 
 };
 
