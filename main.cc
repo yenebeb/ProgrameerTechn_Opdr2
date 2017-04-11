adaffeb37e8aeacdd7128307df257c4c8f249489
Michiel Vrins <Michiel_678@hotmail.com>
2017-04-11 16:19:14 +0200

    fuckery

--- main.cc
+++ main.cc
@@ -7,7 +7,32 @@
 
 int main()
 {
+    // sheet initialization
+    Sheet sheet(5, 5);
+    int k = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            Cell *y = sheet.getCell(i, j);
+            std::stringstream ss;
+            ss << k;
+            std::string s(ss.str());
+            std::string x = std::string(s);
+            k++;
+            sheet.getCell(i, j)->setpointer(x);
+        }
+    }
+    
+    Cell *c = sheet.getCell(2,2);
+    cout << c->readpointer()->stringValue() << endl;
 
+
+  //  return 0;
+
+
+    
+    // ui
     static const int lines(24);
     static const int cols(160);
 
@@ -18,66 +43,71 @@ int main()
     /* Maak een venster, grootte lines x cols */
     WINDOW *win = newwin(lines, cols, 0, 0);
     keypad(win, TRUE); /* Enable keypad input */
-    attr_t old_attr; /* Huidige settings onthouden */
+    attr_t old_attr;   /* Huidige settings onthouden */
     short old_pair;
     int q = 1;
     wattr_get(win, &old_attr, &old_pair, NULL);
     char topHeaderChar = 'A';
     const char *topHeader = &topHeaderChar;
-    for(int i = 0; i <= 24; i++){
-        for(int j = 0; j <= 80; j++){
-            wattron(win, A_STANDOUT);    
+    for (int i = 0; i < 24; i++)
+    {
+        for (int j = 0; j <= 80; j++)
+        {
+            wattron(win, A_STANDOUT);
             wmove(win, i, j);
 
-            if(i ==0){
-                if(j%8 == 2 && j > 2){
+            if (i == 0)
+            {
+                if (j % 8 == 2 && j > 2)
+                {
                     waddstr(win, topHeader);
                     topHeaderChar++;
                 }
-                else if(j > 7){
+                else if (j > 7)
+                {
                     waddstr(win, " ");
                 }
                 refresh();
-
             }
-            else if(j == 0){
-           //     wattr_set(win, old_attr, old_pair, NULL); /* Oude settings terugzetten */
+            else if (j == 0)
+            {
+                //     wattr_set(win, old_attr, old_pair, NULL); /* Oude settings terugzetten */
                 string s = "   ";
                 s += to_string(q);
-                for(int k = s.size(); k < 8; k++){
+                for (int k = s.size(); k < 8; k++)
+                {
                     s += " ";
                 }
                 //printw("test"); // << test printw
-                const char* x = s.c_str();
+                const char *x = s.c_str();
                 waddstr(win, x);
                 q++;
                 refresh();
             }
-            else if(i==5 && j == 30) {
+            else if (i == 5 && j == 30)
+            {
                 ostringstream os;
                 os << "test";
                 string begin = os.str();
                 const char *c = begin.c_str();
                 waddstr(win, c);
 
-                refresh();            
-
+                refresh();
             }
-
         }
         printw("\n");
     }
     /* Verplaats cursor rij 10, kolom 20 */
-   
+
     /* Nogmaals, maar nu op een achtergrond */
- 
-   // wattr_get(win, &old_attr, &old_pair, NULL);
+
+    // wattr_get(win, &old_attr, &old_pair, NULL);
 
     wattron(win, A_STANDOUT);
     wmove(win, 0, 0);
     //waddstr(win, "HELLO WORLD!!!");
     wattr_set(win, old_attr, old_pair, NULL); /* Oude settings terugzetten */
-    Sheet sheet(5, 5);
+
 
     Range a("A2:B5", &sheet);
     /* 
@@ -86,10 +116,11 @@ int main()
     string begin = os.str();
     const char *c = begin.c_str();
     waddstr(win, c); */
-    
+
     /* Wacht tot er op enter wordt gedrukt */
     int ch;
-    while ((ch = wgetch(win)) != '\n'){
+    while ((ch = wgetch(win)) != '\n')
+    {
         //mousemask();
     }
 
@@ -98,25 +129,24 @@ int main()
     endwin();
     return 0;
     cout << endl
-	 << endl
-	 << endl;
-
+         << endl
+         << endl;
 
     cout << "end=" << a.getEnd().getKolomnummer() << a.getEnd().getRijnummer() << endl;
 
-    int k = 0;
+
     for (int i = 0; i < 5; i++)
     {
-	for (int j = 0; j < 5; j++)
-	{
-	    Cell *y = sheet.getCell(i, j);
-	    std::stringstream ss;
-	    ss << k;
-	    std::string s(ss.str());
-	    std::string x = std::string(s);
-	    k++;
-	    sheet.getCell(i, j)->setpointer(x);
-	}
+        for (int j = 0; j < 5; j++)
+        {
+            Cell *y = sheet.getCell(i, j);
+            std::stringstream ss;
+            ss << k;
+            std::string s(ss.str());
+            std::string x = std::string(s);
+            k++;
+            sheet.getCell(i, j)->setpointer(x);
+        }
     }
     /*
     for (int i = 0; i < 5; i++)
@@ -148,10 +178,10 @@ int main()
     RangeIterator rir = range.begin();
     for (int i = 0; i < 8; i++)
     {
-	++rir;
-	Cell *z = &*rir;
-	cout << rir.getOffset();
-	unique_ptr<CellValueBase> test = move(z->readpointer());
-	cout << "value=" << test->stringValue() << " <<<< ";
+        ++rir;
+        Cell *z = &*rir;
+        cout << rir.getOffset();
+        unique_ptr<CellValueBase> test = move(z->readpointer());
+        cout << "value=" << test->stringValue() << " <<<< ";
     }
 }
