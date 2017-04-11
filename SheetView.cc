76e407aa89be5b57bbc46d6be9aa5d0e7131085e
Michiel Vrins <Michiel_678@hotmail.com>
2017-04-11 17:48:31 +0200

    dubbele for is footsie

--- SheetView.cc
+++ SheetView.cc
@@ -7,71 +7,73 @@
 #include "Sheet.h"
 
 using namespace std;
-SheetView::SheetView(int lines, int cols){
-	initscr();
+SheetView::SheetView(int lines, int cols)
+{
+    initscr();
     raw();
     noecho();
-	win = newwin(lines, cols, 0, 0);
-
-
+    win = newwin(lines, cols, 0, 0);
 }
 
-void SheetView::tekenheaders(){
+void SheetView::tekenheaders()
+{
     int q = 1;
     char topHeaderChar = 'A';
     const char *topHeader = &topHeaderChar;
-    for(int i = 0; i < 24; i++){
-        for(int j = 0; j <= 80; j++){
-            wattron(win, A_STANDOUT);    
-            wmove(win, i, j);
+    {
+        wattron(win, A_STANDOUT);
+        wmove(win, 0, 0);
 
-            if(i ==0){
-                if(j%8 == 2 && j > 2){
-                    waddstr(win, topHeader);
-                    topHeaderChar++;
-                }
-                else if(j > 7){
-                    waddstr(win, " ");
-                }
-                refresh();
-
+        for (int j = 0; j <= 80; j++)
+        {
+            wmove(win, 0, j);
+            if (j % 8 == 2 && j > 2)
+            {
+                waddstr(win, topHeader);
+                topHeaderChar++;
             }
-            else if(j == 0){
-                string s = "   ";
-                s += to_string(q);
-                for(int k = s.size(); k < 8; k++){
-                    s += " ";
-                }
-                const char* x = s.c_str();
-                waddstr(win, x);
-                q++;
+            else if (j > 7)
+            {
+                waddstr(win, " ");
+            }
+        }
+        for (int i = 1; i < 24; i++)
+        {
+            wmove(win, i, 0);
+            string s = "   ";
+            s += to_string(q);
+            for (int k = s.size(); k < 8; k++)
+            {
+                s += " ";
+            }
+            const char *x = s.c_str();
+            waddstr(win, x);
+            q++;
+            refresh();
+        }
+    }
+}
+
+void SheetView::tekeninh(Sheet *sheet)
+{
+    for (int i = 0; i < 24; i++)
+    {
+        for (int j = 0; j <= 80; j++)
+        {
+            wmove(win, i, j);
+            if ((j - 2) % 8 == 0 && j > 7 && i > 0)
+            {
+                Cell *d = sheet->getCell(i - 1, (j - 8) / 8);
+                string test = d->getString();
+                const char *c = test.c_str();
+                waddstr(win, c);
                 refresh();
             }
         }
     }
 }
 
-void SheetView::tekeninh(Sheet* sheet){
-    for(int i = 0; i < 24; i++){
-        for(int j = 0; j <= 80; j++){
-            wmove(win, i, j);
-            if(i != 0 && (j-2) % 8 == 0 && j > 7){
-                Cell *c = sheet->getCell(2,2);
-                string begin = c->getString();
-            }
-            else if((j-2) % 8 == 0 && j > 7){
-
-                    Cell *d = sheet->getCell(i-1,(j-8)/8);
-                    string test  = d->getString();
-                    const char *c = test.c_str();
-                    waddstr(win,c);
-                    refresh();
-            }
-        }
-    }
-}
-
-WINDOW* SheetView::getWindow(){
-	return win;
-
+WINDOW *SheetView::getWindow()
+{
+    return win;
 }
